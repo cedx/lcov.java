9fb6d8e0c2187bd89709a2248e45cb9dacdd3f70
Cédric Belin <cedric@belin.io>
2023-07-24 12:46:20 +0200

    Fix the code coverage

--- scripts/Test.java
+++ scripts/Test.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 /**
  * Runs the test suite.
@@ -23,13 +27,23 @@ class Test {
 	 * Script entry point.
 	 * @param args The command line arguments.
 	 */
-	public static void main(String... args) throws InterruptedException, IOException {
+	public static void main(String... args) throws InterruptedException, IOException, ParserConfigurationException, SAXException {
 		exec("java scripts/Build.java --debug");
 
 		var environment = Map.of("CLASSPATH", getClassPath(Path.of("bin")));
-		var pkgPath = pack.replace('.', '/');
+		var pkgPath = Path.of(pack.replace('.', '/'));
 		shellExec("javac -d bin -g -Xlint:all,-path,-processing test/%s/*.java".formatted(pkgPath), environment);
-		System.exit(exec("java org.junit.platform.console.ConsoleLauncher --select-package=" + pack, environment));
+
+		var jacocoJar = "jacocoagent.jar";
+		var outputDirectory = Path.of("var");
+		extractJacocoJar(jacocoJar, outputDirectory);
+
+		var jacocoExec = outputDirectory.resolve("jacoco.exec");
+		var jacocoAgent = "-javaagent:%s=append=false,destfile=%s,includes=%s.*".formatted(outputDirectory.resolve(jacocoJar), jacocoExec, pack);
+		var exitCode = exec("java %s org.junit.platform.console.ConsoleLauncher --select-package=%s".formatted(jacocoAgent, pack), environment);
+
+		if (exitCode != 0) System.exit(exitCode);
+		generateCoverageReport(jacocoExec, Path.of("bin").resolve(pkgPath));
 	}
 
 	/**
@@ -55,6 +69,48 @@ class Test {
 		var process = Runtime.getRuntime().exec(Objects.requireNonNull(command), variables.toArray(String[]::new));
 		Stream.concat(process.errorReader().lines(), process.inputReader().lines()).parallel().forEach(System.out::println);
 		return process.waitFor();
+	}
+
+	/**
+	 * Extracts the JaCoCo Agent archive into the specified output directory.
+	 * @param jarName The file name of the JaCoCo Agent archive.
+	 * @param outputDirectory The path of the directory where to extract the agent.
+	 */
+	private static void extractJacocoJar(String jarName, Path outputDirectory)
+	throws InterruptedException, IOException, ParserConfigurationException, SAXException {
+		var outputFile = outputDirectory.resolve(jarName);
+		if (Files.exists(outputFile)) return;
+
+		var xml = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File("ivy.xml"));
+		xml.getDocumentElement().normalize();
+
+		var dependencies = xml.getElementsByTagName("dependency");
+		var version = Stream.iterate(0, index -> index < dependencies.getLength(), index -> index + 1)
+			.map(index -> (Element) dependencies.item(index))
+			.filter(dependency -> dependency.getAttribute("name").equals("org.jacoco.agent"))
+			.findFirst()
+			.orElseThrow()
+			.getAttribute("rev");
+
+		exec("jar --extract --file=lib/org.jacoco/org.jacoco.agent/jars/org.jacoco.agent-%s.jar %s".formatted(version, jarName));
+		Files.move(Path.of(jarName), outputFile);
+	}
+
+	/**
+	 * Generates the coverage report from the specified JaCoCo execution file and class directory.
+	 * @param execFile The path of the JaCoCo execution file.
+	 * @param classDirectory The path of the directory containing the compiled classes.
+	 */
+	private static void generateCoverageReport(Path execFile, Path classDirectory) throws InterruptedException, IOException {
+		Files.walk(Objects.requireNonNull(classDirectory))
+			.filter(file -> file.endsWith("Test.class"))
+			.map(Path::toFile)
+			.forEach(File::delete);
+
+		exec("java org.jacoco.cli.internal.Main report %s --classfiles=%s --sourcefiles=src --xml=var/coverage.xml".formatted(
+			Objects.requireNonNull(execFile),
+			classDirectory
+		), Map.of("CLASSPATH", getClassPath(Path.of("bin"))));
 	}
 
 	/**
